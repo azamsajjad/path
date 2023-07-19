1f0c81415553dfcfdd5916f81fe44d1df52211c8
Syed Azam Sajjad <syedsajjad.rh@gmail.com>
2023-07-20 04:32:38 +0500

    20072023

--- 2-RHCSA/2.20-Shell_Scripts.sh
+++ 2-RHCSA/2.20-Shell_Scripts.sh
@@ -49,3 +49,26 @@ done command is used to close these loops
 
 VIM STOP INDENTATION
 :setl nocin nosi noai inde=
+
+
+ANSIBLE stopping dowmloads
+#!/bin/sh
+
+# Remove transaction_list, otherwise playbook will not check url again.
+rm -f /home/ansible/transaction_list > /dev/null;
+
+# Is httpd running?
+nc -z -w 50ms apps.l33t.com 80 > /dev/null
+
+
+# Switch states depending on if it is up or down
+if [ $? -ne 0 ];
+then
+  echo -n "Starting apps.l33t.com...";
+  ansible node1 -b -m service -a "name=httpd state=started" > /dev/null;
+  echo "done."
+else
+  echo -n "Stopping apps.l33t.com...";
+  ansible node1 -b -m service -a "name=httpd state=stopped" > /dev/null;
+  echo "done."
+fi
---
+++ 3-RHCE/3.80-Ansible_Tower.sh
@@ -0,0 +1,12 @@
+ANSIBLE_CONTROLLER
+inventory file
+define 4 passwords and run setup.sh
+
+[all:vars]
+admin_password='redhat'
+
+pg_password='redhat'
+#pg=postgresql database
+automationhub_admin_password='redhat'
+automationhub_pg_password='redhat'
+
