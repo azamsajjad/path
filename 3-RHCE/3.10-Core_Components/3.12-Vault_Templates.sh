90417363e615207ef48b9045b886b4d77a74b3ba
Syed Azam Sajjad <syedsajjad.rh@gmail.com>
2023-07-25 15:44:13 +0500

    25072023

--- 3-RHCE/3.10-Core_Components/3.12-Vault_Templates.sh
+++ 3-RHCE/3.10-Core_Components/3.12-Vault_Templates.sh
@@ -290,69 +290,69 @@ DocumentRoot "{{ content_dir }}"
 # Further relax access to the default document root:
 <Directory "{{ content_dir }}">
     #
-    Options Indexes FollowSymLinks
-    AllowOverride None
-    # Controls who can get stuff from this server.
-    Require all granted
-</Directory>
-# DirectoryIndex: sets the file that Apache will serve if a directory
-<IfModule dir_module>
-    DirectoryIndex index.html
-</IfModule>
-# The following lines prevent .htaccess and .htpasswd files from being
-<Files ".ht*">
-    Require all denied
-</Files>
-# ErrorLog: The location of the error log file.
-ErrorLog "logs/error_log"
-# LogLevel: Control the number of messages logged to the error_log.
-LogLevel warn
+#     Options Indexes FollowSymLinks
+#     AllowOverride None
+#     # Controls who can get stuff from this server.
+#     Require all granted
+# </Directory>
+# # DirectoryIndex: sets the file that Apache will serve if a directory
+# <IfModule dir_module>
+#     DirectoryIndex index.html
+# </IfModule>
+# # The following lines prevent .htaccess and .htpasswd files from being
+# <Files ".ht*">
+#     Require all denied
+# </Files>
+# # ErrorLog: The location of the error log file.
+# ErrorLog "logs/error_log"
+# # LogLevel: Control the number of messages logged to the error_log.
+# LogLevel warn
 
-<IfModule log_config_module>
-    # The following directives define some format nicknames for use with
-    LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
-    LogFormat "%h %l %u %t \"%r\" %>s %b" common
+# <IfModule log_config_module>
+#     # The following directives define some format nicknames for use with
+#     LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+#     LogFormat "%h %l %u %t \"%r\" %>s %b" common
 
-    <IfModule logio_module>
-      # You need to enable mod_logio.c to use %I and %O
-      LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %I %O" combinedio
-    </IfModule>
+#     <IfModule logio_module>
+#       # You need to enable mod_logio.c to use %I and %O
+#       LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %I %O" combinedio
+#     </IfModule>
 
-    CustomLog "logs/access_log" combined
-</IfModule>
+#     CustomLog "logs/access_log" combined
+# </IfModule>
 
-<IfModule alias_module>
-    ScriptAlias /cgi-bin/ "/var/www/cgi-bin/"
+# <IfModule alias_module>
+#     ScriptAlias /cgi-bin/ "/var/www/cgi-bin/"
 
-</IfModule>
-# "/var/www/cgi-bin" should be changed to whatever your ScriptAliased
-# CGI directory exists, if you have that configured.
-<Directory "/var/www/cgi-bin">
-    AllowOverride None
-    Options None
-    Require all granted
-</Directory>
+# </IfModule>
+# # "/var/www/cgi-bin" should be changed to whatever your ScriptAliased
+# # CGI directory exists, if you have that configured.
+# <Directory "/var/www/cgi-bin">
+#     AllowOverride None
+#     Options None
+#     Require all granted
+# </Directory>
 
-<IfModule mime_module>
-    # TypesConfig points to the file containing the list of mappings from
-    # filename extension to MIME-type.
-    TypesConfig /etc/mime.types
+# <IfModule mime_module>
+#     # TypesConfig points to the file containing the list of mappings from
+#     # filename extension to MIME-type.
+#     TypesConfig /etc/mime.types
 
-    # AddType allows you to add to or override the MIME configuration
-    # file specified in TypesConfig for specific file types.
-    AddType application/x-compress .Z
-    AddType application/x-gzip .gz .tgz
-    AddType text/html .shtml
-    AddOutputFilter INCLUDES .shtml
-</IfModule>
+#     # AddType allows you to add to or override the MIME configuration
+#     # file specified in TypesConfig for specific file types.
+#     AddType application/x-compress .Z
+#     AddType application/x-gzip .gz .tgz
+#     AddType text/html .shtml
+#     AddOutputFilter INCLUDES .shtml
+# </IfModule>
 
-AddDefaultCharset UTF-8
+# AddDefaultCharset UTF-8
 
-<IfModule mime_magic_module>
-    # The mod_mime_magic module allows the server to use various hints from the
-    # contents of the file itself to determine its type.  The MIMEMagicFile
-    MIMEMagicFile conf/magic
-</IfModule>
+# <IfModule mime_magic_module>
+#     # The mod_mime_magic module allows the server to use various hints from the
+#     # contents of the file itself to determine its type.  The MIMEMagicFile
+#     MIMEMagicFile conf/magic
+# </IfModule>
 # Defaults if commented: EnableMMAP On, EnableSendfile Off
 #EnableMMAP off
 EnableSendfile on
