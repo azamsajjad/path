7ef552b2f5e7216ffc0963cb9aae2d9c8954dbdf
azamsajjad <azamsaws@gmail.com>
2024-04-05 16:20:13 +0500

    05042024

--- 9-GIT-ELK/9.14-Git_branches.sh
+++ 9-GIT-ELK/9.14-Git_branches.sh
@@ -1,5 +1,18 @@
 git diff
 shows difference between old committed file and modified file
 
+Workflow - from Ansible branch
+  795  git checkout -b 05042024
+  797  vim README.md
+  799  git add README.md
+  800  git commit -m "05042024"
+  801  git checkout ansible
+  802  vim README.md 
+  805  git merge 05042024
+  806  git branch -d 05042024
+  810  git push -u origin ansible
+  811  eval "`ssh-agent -s`"
+  812  ssh-add ~/.ssh/id_ed25519_git_azam_sajjad
+  813  git push -u origin ansible
 
 
